2ab44d527dca8ea87892a3b0ac1c6d49a1bf2114
Rennan <Rennan@gmail.com>
2022-03-23 14:36:19 -0300

    Adicionando anotaçoes jpa nas classes

--- src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
+++ src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
@@ -1,5 +1,47 @@
 package br.com.etechoracio.boa_viagem.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.com.etechoracio.boa_viagem.enums.CategoriaEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_GASTO")
 public class Gasto {
 
+	@Id
+	@Column(name = "ID_GASTO")
+	private Long id;
+	
+	@Column(name = "TX_DESCRICAO")
+	private String descricao;
+	
+	@Column(name = "TX_LOCAL")
+	private String local;
+	
+	@Column(name = "TP_CATEGORIA")
+	@Enumerated(EnumType.STRING)
+	private CategoriaEnum categoria;
+	
+	@Column(name = "DT_DATA")
+	private LocalDate data;
+	
+	@Column(name = "VLR_GASTO")
+	private Double valor;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID_VIAGEM")
+	private Viagem viagem;
 }
--- src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
+++ src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
@@ -1,5 +1,45 @@
 package br.com.etechoracio.boa_viagem.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.etechoracio.boa_viagem.enums.TipoViagemEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_VIAGEM")
 public class Viagem {
 
+	@Id
+	@Column(name = "ID_VIAGEM")
+	private Long id;
+	
+	@Column(name = "TX_DESTINO")
+	private String destino;
+	
+	@Column(name = "TP_VIAGEM")
+	@Enumerated(EnumType.STRING)
+	private TipoViagemEnum viagem;
+	
+	@Column(name = "DT_CHEGADA")
+	private LocalDate chegada;
+	
+	@Column(name = "DT_SAIDA")
+	private LocalDate saida;
+	
+	@Column(name = "VLR_ORCAMENTO")
+	private double orcamento;
+	
+	@Column(name = "QTD_PESSOAS")
+	private int pessoas;
+	
 }
